6520dca51169667c637185991e40c3c875d64045
renaneliakim1 <renaneliakim1@gmail.com>
2025-01-02 21:45:27 -0300

    feat(indexDia01.js) : add question

--- indexDia01.js
+++ indexDia01.js
@@ -1,4 +1,4 @@
-/* 1--------------------------------------------------------------
+/* 1-Declare uma variável para armazenar o preço de um produto e outra para o desconto. Calcule o preço final e mostre no console.
 
 let produto= parseFloat(prompt("Digite o preço do produto:"));
 // tratamento para que seja inserido apenas numeros 
@@ -19,13 +19,13 @@ alert ( "O preço final do produto é: R$ " + precoFinal.toFixed(2)  )
 // toFixed - permite exibir as casas decimais  */
 
 
-/* 2------------------------------------------
+/* 2-Crie um array com três de suas comidas favoritas e exiba a segunda comida na lista.
 
 let coomidasFavoritas = [ "carne", "feijao", "arroz"]
 console.log(coomidasFavoritas[2]) */
 
 
-/* //3--------------------------------------------------------
+/* //3-Crie um objeto que represente um livro, com propriedades como titulo, autor e ano. Mostre no console o título e o autor.
 const livro = {
     titulo: "Entendendo Algoritmos",
     autor: "Renã Eliakim",
@@ -42,7 +42,7 @@ console.log(`${livro.titulo}, escrito por ${livro.autor}, publicado em ${livro.a
 
 
 
-/* //4----------------------------------------------------------------
+/* //4-Declare uma variável estaLogado com o valor false. Depois, mude o valor para true e imprima a mensagem: "Status de login: [valor da variável]".
 
 let estaLogado = false
 estaLogado = true
@@ -52,7 +52,7 @@ console.log(`Status do login: ${estaLogado}`);
  */
 
 
-//5-------------------------------------------------------------------------
+//5-Crie uma variável constante chamada PI com o valor 3.14. Use-a para calcular a área de um círculo com raio 5 e mostre o resultado.
 /* 
 const pi = 3.14;
 
